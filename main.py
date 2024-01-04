7c9ae21e2cccec3053f7ce09bd0925c952549dd5
Grant Schwidder <gschwid@gmail.com>
2024-01-04 16:16:44 -0600

    created the basic window, following tutorial on how to use tkinter

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk() # Creates the window
+
+myLabel = Label(root, text="Hello World!") # Creating label widget
+
+myLabel.pack() # Shoving it on the screen
+
+root.mainloop() # Displays the window, loops constantly
