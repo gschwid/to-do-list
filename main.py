544c64764bd91791f90747da695a7a19cb4bed4c
Grant Schwidder <gschwid@gmail.com>
2024-01-07 21:18:48 -0600

    Buttons next to tasks delete them

--- main.py
+++ main.py
@@ -10,14 +10,21 @@ root.title("Grant's task manager")
 # Function that button calls to add tasks to the window.
 def createTasks():
     global rowCount
+    dict = {} # For storing each of the tasks
     task = e.get()
     if len(task) != 0 and task != "Enter your tasks: ":
         myTask = Label(text=task) 
         myTask.grid(row=rowCount, column=0) # Adding task to the to-do-list
-        taskButton = Button(background="black")
+        dict[rowCount] = myTask # Adds the task to the dictionary, allowing it to be deleted later.
+        taskButton = Button(background="black", command=lambda: removeTask(dict))
         taskButton.grid(row=rowCount, column=1) # Adding button to the to-do-list that will remove the task
         clear(e) # Clears what the user inputs as a task.
         rowCount += 1 
+
+def removeTask(tasks):
+    global rowCount
+    task:Label= tasks.get(rowCount - 1) # Accesses the needed task
+    task.destroy()
 
 # Clears what is in the entry bar
 def clear(entry:Entry):
